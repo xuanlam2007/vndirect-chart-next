c315d9351cade7d6c403a9693032d8b1eb66f87f
Xuân Lâm <hoangxuanlam2007@outlook.com>
2026-09-18 13:30:36 +0700

    fix(chart): remove inline search clear action | bỏ nút xóa trong ô tìm kiếm

--- components/chart/layout/SymbolSearchModal.tsx
+++ components/chart/layout/SymbolSearchModal.tsx
@@ -350,12 +350,12 @@ export function SymbolSearchModal({
           <span className="symbol-search-modal__search-icon" aria-hidden="true">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
+              viewBox="0 0 18 18"
               width="18"
               height="18"
-              fill="currentColor"
+              fill="none"
             >
-              <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
+              <path stroke="currentColor" d="M12.4 12.5a7 7 0 1 0-4.9 2 7 7 0 0 0 4.9-2zm0 0 5.101 5" />
             </svg>
           </span>
           <input
@@ -368,27 +368,6 @@ export function SymbolSearchModal({
             autoComplete="off"
             spellCheck={false}
           />
-          {query && (
-            <button
-              type="button"
-              className="symbol-search-modal__clear-btn"
-              onClick={() => {
-                setQuery("");
-                inputRef.current?.focus();
-              }}
-              aria-label="Xóa nội dung tìm kiếm"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="16"
-                height="16"
-                fill="currentColor"
-              >
-                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
-              </svg>
-            </button>
-          )}
         </div>
 
         <div className="symbol-search-modal__table-header">
